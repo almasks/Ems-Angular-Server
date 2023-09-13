05fa952f2002040f87196722ac4df558cbdacc73
Almas K S <mailmealmasks@gmail.com>
2023-09-13 10:54:48 +0530

    employee portal server

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer =require('json-server')
+const server =jsonServer.create()
+const middleware =jsonServer.defaults()
+const router = jsonServer.router('db.json')
+const port =3005 ||process.env.PORT
+server.use(middleware)
+server.use(router)
+server.listen(port,()=>{
+    console.log('employee portal server started at port no'+port)
+})
